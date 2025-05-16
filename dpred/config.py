0ce514f51e78691c6740496bdba2a2aeb46c2415
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-05-16 08:25:13 +0545

    Fixed Ci-Cd Pipeline

--- dpred/config.py
+++ dpred/config.py
@@ -1,19 +1,15 @@
+# dpred/config.py
+
 from pathlib import Path
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     dataset_path: Path
     model_path: Path
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
-
-project_root = Path(__file__).parent.parent.resolve()
-
-settings.dataset_path = (project_root / settings.dataset_path).resolve()
-settings.model_path = (project_root / settings.model_path).resolve()
